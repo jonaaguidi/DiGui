16ba06334929bdc936954724ec86f598669a3314
Jonathan Guidi <jguidi.info@gmail.com>
2023-07-21 15:27:08 -0300

    About me Component

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { fadeIn, textVariant } from "../utils/motion";
+import { styles } from '../styles';
+import Image from 'next/image';
+import JonaIA from "../../public/JonaIA.jpg";
 
 const About = () => {
   return (
-    <div className='w-full mx-auto'>
-    <h2 className='text-center font-bold text-2xl'>Mi Nombre es Jonathan Guidi</h2>
-    </div>
-  )
-}
+    <motion.div id='about' className='w-full px-10 mt-4 flex flex-col items-center gap-12 md:flex-row justify-center'>
+      <motion.div className="flex flex-col" variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+          I'm a skilled software developer with experience in TypeScript and
+          JavaScript, and expertise in frameworks like React, Node.js, and
+          Three.js. I'm a quick learner and collaborate closely with clients to
+          create efficient, scalable, and user-friendly solutions that solve
+          real-world problems. Let's work together to bring your ideas to life!
+        </motion.p>
+      </motion.div>
 
-export default About
+      <div className='animate-pulse2 bg-[#DD1D43] rounded-full p-6 flex justify-center items-center md:ml-6 md:order-first '>
+        <Image className='rounded-full' src={JonaIA} alt='Avatar' width={300} height={300} />
+      </div>
+    </motion.div>
+  );
+};
+
+export default About;
--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,18 +1,16 @@
-import { motion } from "framer-motion"
 import { styles } from "../styles"
 import { ComputersCanvas } from "./canvas"
+import Scroll from "./Scroll"
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
-      <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
-      >
+    <>
+    <section className="relative w-full h-screen mx-auto">
+      <div className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#DD1D43]' />
           <div className='w-1 sm:h-80 h-40 red-gradient' />
         </div>
-
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, Im <span className='text-[#DD1D43]'>Jonathan🤝</span>
@@ -22,33 +20,18 @@ const Hero = () => {
           </p>
           <div className="flex gap-5 mt-6">
             <button class="bg-[#DD1D43] text-white py-2 px-4 rounded-2xl cursor-pointer hover:btn-hero z-50">
-              <a target="no_blank" rel="noreferrer" href="https://drive.google.com/file/d/1hJge5a3NNa_Eg18tOPuIlOvBf0h2wZ4C/view?usp=drive_link">Ver CV 📄</a>
+              <a target="no_blank" rel="noreferrer" href="https://drive.google.com/file/d/1hJge5a3NNa_Eg18tOPuIlOvBf0h2wZ4C/view?usp=drive_link">CV 📄</a>
             </button>
             <button class="bg-[#DD1D43] text-white py-2 px-4 rounded-2xl cursor-pointer hover:btn-hero z-50">
-              <a href="#contact">Contacto 📩</a>
+              <a href="#contact">Contact 📩</a>
             </button>
           </div>
         </div>
       </div>
       <ComputersCanvas />
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-        <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.dev
-              animate={{
-                y: [0, 24, 0]
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop"
-              }}
-              className="w-3 h-3 rounded-full bg-secondary mb-1"
-            />
-          </div>
-        </a>
-      </div>
+      <Scroll />
     </section>
+    </>
   )
 }
 
