412b8b48315a44897172864f2531fe3fed2da854
Jonathan Guidi <jguidi.info@gmail.com>
2023-07-30 22:06:41 -0300

    fix

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -12,11 +12,11 @@ const About = () => {
         <p className={styles.sectionSubText}>Introducción</p>
         <h2 className={styles.sectionHeadText}>Sobre Mi</h2>
         <p className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
-          I'm a skilled software developer with experience in TypeScript and
+          Im a skilled software developer with experience in TypeScript and
           JavaScript, and expertise in frameworks like React, Node.js, and
-          Three.js. I'm a quick learner and collaborate closely with clients to
+          Three.js. Im a quick learner and collaborate closely with clients to
           create efficient, scalable, and user-friendly solutions that solve
-          real-world problems. Let's work together to bring your ideas to life!
+          real-world problems. Let s work together to bring your ideas to life!
         </p>
         <div className='mt-8 flex gap-4'>
           <a href='https://www.linkedin.com/in/jonathanguidi/' target='blank' rel='noreferrer'>
